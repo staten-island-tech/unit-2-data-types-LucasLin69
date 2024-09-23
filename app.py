143509ce7c6fbd379f8619ae1366f8946c03a1b0
LucasLin69 <lucasl92@nycstudents.net>
2024-09-23 10:12:14 -0400

    OHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH MYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY PCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC

--- app.py
+++ app.py
@@ -21,13 +21,29 @@ elif service == good:
     print("20% Tip")
 elif service == great:
     print("25% Tip") """
-import math
-positive_infinity = math.inf
-number = 12
+""" number = 112
 def find_factors(n):
-    quotient, remainder = divmod(n,math.inf)
-    if remainder > (0):
-        print(remainder)
-        
-print("7")
-print(find_factors(number))
+    factors = []
+    for i in range(1,n+1):
+        if n % i == 0:
+            factors.append(i)
+    return factors
+print(find_factors(number)) """
+number = 20
+SUPAAAN = 100
+a=number
+b=SUPAAAN
+def GCF(a,b):
+    factors = []
+    for i in range(1,a+1):
+        if a % i == 0:
+            factors.append(i)
+    return factors
+    factor = []
+    for i in range(1,b+1):
+        if b % i == 0:
+            factor.append(i)
+    return factor
+    if 
+print(GCF(number,SUPAAAN))
+
