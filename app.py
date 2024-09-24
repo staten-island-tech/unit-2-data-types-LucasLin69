b9a4b1ecfddaae80079790e0c546d546f5a12a17
LucasLin69 <lucasl92@nycstudents.net>
2024-09-24 10:13:35 -0400

    baller

--- app.py
+++ app.py
@@ -1,49 +1,5 @@
-""" x = 99
-quotient, remainder = divmod(x,2)
-if remainder == 1:
-    print("Odd")
-else:
-    print("Even") """
-""" def bad(q):
-    q = ("0%")
-def okay(w):
-    w = ("15%")
-def good(e):
-    e = ('20%')
-def great(r):
-    r = ("25%")
-service = good
-if service == bad:
-    print("0%")
-elif service == okay:
-    print("15% Tip")
-elif service == good:
-    print("20% Tip")
-elif service == great:
-    print("25% Tip") """
-""" number = 112
-def find_factors(n):
-    factors = []
-    for i in range(1,n+1):
-        if n % i == 0:
-            factors.append(i)
-    return factors
-print(find_factors(number)) """
-number = 20
-SUPAAAN = 100
-a=number
-b=SUPAAAN
-def GCF(a,b):
-    factors = []
-    for i in range(1,a+1):
-        if a % i == 0:
-            factors.append(i)
-    return factors
-    factor = []
-    for i in range(1,b+1):
-        if b % i == 0:
-            factor.append(i)
-    return factor
-    if 
-print(GCF(number,SUPAAAN))
-
+import math
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+gcf = math.gcd(num1, num2)
+print(f"The greatest common factor of {num1} and {num2} is {gcf}")
