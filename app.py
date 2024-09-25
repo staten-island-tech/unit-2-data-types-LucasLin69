be660edfe58f13f65cc44306afe1e73bd3e05329
LucasLin69 <lucasl92@nycstudents.net>
2024-09-25 10:14:02 -0400

    pcpc

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import math
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 gcf = math.gcd(num1, num2)
-print(f"The greatest common factor of {num1} and {num2} is {gcf}")
+print(f"The greatest common factor of {num1} and {num2} is {gcf}.")
