8e230b1ac4e423164fcef07b66e0d5eb2d0d3cb5
LucasLin69 <lucasl92@nycstudents.net>
2024-09-26 10:09:31 -0400

    ball

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-import math
+""" import math
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 gcf = math.gcd(num1, num2)
 print(f"The greatest common factor of {num1} and {num2} is {gcf}.")
+"""
+verb = input("Say a verb: ")
+noun = input("Say a noun: ")
+past_tense_verb = input("Say a past tense verb: ")
+number = input("Say a number: ")
+celebrity = input("Say a celebrity: ")
+MadLibs = (f"One day, I decided to {verb} to the park. To my surprise, I saw a {noun} there! Suddenly, I {past_tense_verb} and noticed that there were {number} people gathered around. Among them was none other than {celebrity}! It was an unforgettable day!")
+print(f"This is your story: {MadLibs}")
+
